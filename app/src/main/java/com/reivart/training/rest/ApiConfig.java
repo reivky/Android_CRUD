381c53c85e385d47fe75ea4176a32d36eb3cb17f
reivky <reivky01@gmail.com>
2020-05-19 16:34:43 +0700

    crud

---
+++ app/src/main/java/com/reivart/training/rest/ApiConfig.java
@@ -0,0 +1,15 @@
+package com.reivart.training.rest;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiConfig {
+    public static ApiService getApiService(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.43.81/lesKohDerry/ ")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ApiService service =retrofit.create(ApiService.class);
+        return service;
+    }
+}
